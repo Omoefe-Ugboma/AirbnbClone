c751881e42246b2a46e80afd7c5692ce4403ef2d
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-04 01:41:52 +0100

    Initiating Header and home on the airbnb clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Home from './Home'
+import Header from './Header'
 
 function App() {
   return (
-    <div className="App">
-     <h1> Hey Omoefem, Let's build AIRBNB clone ✔</h1>
+    // BEM
+    <div className="app">
+      <Header/>
+     <Home/>
     </div>
   );
 }
