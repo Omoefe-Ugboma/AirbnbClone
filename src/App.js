86c9b8e03c042bb0abc17a081abf316ebe9c81c5
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-04 01:10:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Hey Omoefem, Let's build AIRBNB clone ✔</h1>
+    </div>
+  );
+}
+
+export default App;
