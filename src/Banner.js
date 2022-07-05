6734fdfb3fbf6051ccf1e8aeca13bede4aba7751
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-05 14:17:30 +0100

    Initiating banner section

---
+++ src/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Banner.css'; 
+function Banner() {
+  return (
+    <div className='banner'>
+       <h1>I am a banner</h1>
+    </div>
+  )
+}
+
+export default Banner
