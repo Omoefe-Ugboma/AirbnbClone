264e19a40591dc609b52cce7116fcd428728adb5
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-06 21:30:48 +0100

    Date picker in progress

--- src/Banner.js
+++ src/Banner.js
@@ -7,7 +7,7 @@ function Banner() {
   return (
     <div className='banner'>
         <div className='banner__search'>
-            {showSearch && <h1>SHOW DATE PICKER</h1>}
+            {showSearch && <Search/>}
           <Button onClick={()=>
           setShowSearch(!showSearch)}
           className='banner__searchButton'
---
+++ src/Search.js
@@ -0,0 +1,11 @@
+ import React from 'react'
+ import './Search.css'
+ function Search() {
+   return (
+     <div className='search'>
+
+     </div>
+   )
+ }
+ 
+ export default Search
