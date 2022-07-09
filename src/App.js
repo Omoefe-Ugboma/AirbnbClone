a7dd3f9334dfa4dca30fb5c7439b4e4dfb383de0
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-09 12:43:12 +0100

    Card section completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="app">
       <Header/>
      <Home/>
+     <Footer/>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -9,36 +9,39 @@ function Home() {
         <Banner/>
         <div className='home__section'>
            <Card 
-           src="https://images.pexels.com/photos/261169/pexels-photo-261169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+           src="https://images.pexels.com/photos/1134176/pexels-photo-1134176.jpeg?auto=compress&cs=tinysrgb&w=600"
            title="Online Experiences"
            description="orem ipsum dolor sit amet consectetur adipisicing elit."
            />
            <Card 
-            src="https://images.pexels.com/photos/774042/pexels-photo-774042.jpeg?auto=compress&cs=tinysrgb&w=600"
-            title="Online Experiences"
+            src="https://images.pexels.com/photos/271618/pexels-photo-271618.jpeg?auto=compress&cs=tinysrgb&w=600"
+            title="Unique Stays"
             description="orem ipsum dolor sit amet consectetur adipisicing elit."
            />
            <Card 
-            src="https://images.pexels.com/photos/262047/pexels-photo-262047.jpeg?auto=compress&cs=tinysrgb&w=600"
-            title="Online Experiences"
+            src="https://images.pexels.com/photos/1268855/pexels-photo-1268855.jpeg?auto=compress&cs=tinysrgb&w=600"
+            title="Entire homes"
             description="orem ipsum dolor sit amet consectetur adipisicing elit."
            />
         </div>
         <div className='home__section'>
         <Card 
-           src="https://images.pexels.com/photos/2096983/pexels-photo-2096983.jpeg?auto=compress&cs=tinysrgb&w=600"
-           title="Online Experiences"
+           src="https://images.pexels.com/photos/26139/pexels-photo-26139.jpg?auto=compress&cs=tinysrgb&w=600"
+           title="3 Bedroom Flat in with rest in heart"
            description="orem ipsum dolor sit amet consectetur adipisicing elit."
+           price="$40/night"
            />
            <Card 
             src="https://images.pexels.com/photos/137090/pexels-photo-137090.jpeg?auto=compress&cs=tinysrgb&w=600"
-            title="Online Experiences"
+            title="Penthouse in London"
             description="orem ipsum dolor sit amet consectetur adipisicing elit."
+            price="$30/night"
            />
            <Card 
-            src="https://images.pexels.com/photos/895555/pexels-photo-895555.jpeg?auto=compress&cs=tinysrgb&w=600"
-            title="Online Experiences"
+            src="https://images.pexels.com/photos/1838554/pexels-photo-1838554.jpeg?auto=compress&cs=tinysrgb&w=600"
+            title="1 Bedroom apartment"
             description="orem ipsum dolor sit amet consectetur adipisicing elit."
+            price="$49/night"
            />
         </div>
     </div>
