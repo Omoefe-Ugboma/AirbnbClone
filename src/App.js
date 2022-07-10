0403a72de201a2c8105028a53de93a52ba562d08
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-10 13:22:28 +0100

    Search route configure and in progress

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import SearchPage from './SearchPage';
+
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
@@ -11,7 +13,19 @@ function App() {
     <div className="app">
       <Router>
         <Header/>
-        <Home/>
+
+         <Switch>         
+           
+           <Route path="/search">
+              <SearchPage />
+           </Route>
+
+           <Route path="/">
+               <Home/>
+           </Route>
+
+         </Switch>
+
         <Footer/>
       </Router>
     </div>
---
+++ src/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SearchPage.css'
+function SearchPage() {
+  return (
+    <div className='searchPage'>
+       <h1>I am a search page</h1>
+    </div>
+  )
+}
+
+export default SearchPage
