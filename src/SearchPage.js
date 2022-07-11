f3602919b7d7865129c865bcd6ddd98f14ada225
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-12 00:03:15 +0100

    Working on the search

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,11 +1,48 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 import './SearchPage.css'
+import SearchResult from './SearchResult'
+
 function SearchPage() {
   return (
     <div className='searchPage'>
-       <h1>I am a search page</h1>
+       <div className='searchPage__info'>
+         <p> 62 stays . 26 august to 30 
+          august . 2 guest</p>
+          <h1>Stays nearby</h1>
+          <Button
+          variant='outlined'>Cancellation
+           Flexibility</Button>
+
+           <Button
+          variant='outlined'>Type of
+           place</Button>
+
+           <Button
+          variant='outlined'>Price</Button>
+
+           <Button
+          variant='outlined'>Rooms and beds</Button>
+
+           <Button
+          variant='outlined'>More filters</Button>
+       </div>
+       <SearchResult 
+       img="https://images.pexels.com/photos/1838554/pexels-photo-1838554.jpeg?auto=compress&cs=tinysrgb&w=600"
+       location="Private room in
+       center of london"
+       title="Stay at this spacious
+       Edwardian House"
+       description="1 guest . 1
+       bedroom . 1 bed . 1.5 shared
+       bathrooms . wifi . kitchen .
+       Free parking . Washing Washing Machine"
+       star={4.73}
+       price="$36 / night"
+       total="$124 total"
+       />
     </div>
   )
 }
 
-export default SearchPage
+export default SearchPage 
